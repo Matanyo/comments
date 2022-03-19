21bbba001d0682f20a44c707d3e02787105262ee
Matanyo <matanyo@gmail.com>
2022-03-19 03:07:16 +0200

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { useEffect } from "react";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { styled } from "@mui/system";
 import { useInfiniteQuery } from "react-query";
 import { fetchCommentsByPage, commentsKey } from "./api";
 import "./App.css";
+
+const CardStyled = styled(Card)`
+  max-width: 500px;
+`;
 
 function App() {
   const { data, isFetching, fetchNextPage, hasNextPage, isFetchingNextPage } =
@@ -24,16 +39,50 @@ function App() {
       {hasNextPage && (
         <button onClick={() => fetchNextPage()}>load more</button>
       )}
+
       {JSON.stringify(hasNextPage)}
       {data && (
         <div>
           {data.pages.map((page, index) => {
             return (
-              <div key={index}>
+              <Grid
+                spacing={2}
+                columnSpacing={2}
+                rowSpacing={2}
+                key={index}
+                container
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+              >
                 {page.map((comment) => {
-                  return <div key={comment.id}>{comment.body}</div>;
+                  return (
+                    <Grid item>
+                      <CardStyled>
+                        <CardHeader
+                          avatar={
+                            <Avatar aria-label="recipe">
+                              {comment.email[0]}
+                            </Avatar>
+                          }
+                          title={comment.name}
+                          subheader={comment.email}
+                        />
+                        <CardContent>
+                          <Typography variant="body2">
+                            {comment.body}
+                          </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                          <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                          </IconButton>
+                        </CardActions>
+                      </CardStyled>
+                    </Grid>
+                  );
                 })}
-              </div>
+              </Grid>
             );
           })}
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
